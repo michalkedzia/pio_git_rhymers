05d44aa894935bb89111625c876022e651bc5eb9
michalkedzia <222463@edu.p.lodz.pl>
2020-12-02 17:36:10 +0100

    1.3.5 Refactoring method names

--- src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
@@ -4,11 +4,11 @@ import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 
 public interface Rhymersfactory {
 
-  public DefaultCountingOutRhymer GetStandardRhymer();
+  public DefaultCountingOutRhymer getStandardRhymer();
 
-  public DefaultCountingOutRhymer GetFalseRhymer();
+  public DefaultCountingOutRhymer getFalseRhymer();
 
-  public DefaultCountingOutRhymer GetFIFORhymer();
+  public DefaultCountingOutRhymer getFIFORhymer();
 
-  public DefaultCountingOutRhymer GetHanoiRhymer();
+  public DefaultCountingOutRhymer getHanoiRhymer();
 }
